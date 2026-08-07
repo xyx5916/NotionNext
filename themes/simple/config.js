7f10eec49055645736f82d72473c16479f54ecc6
xyx5916 <yiduoliuxing@qq.com>
2026-08-07 20:32:03 +0800

    Update config.js

--- themes/simple/config.js
+++ themes/simple/config.js
@@ -1,30 +1,33 @@
-const CONFIG = {
-
+const SIMPLE_CONFIG = {
   SIMPLE_LOGO_IMG: '/Logo.webp',
-  SIMPLE_TOP_BAR: true, // 显示顶栏
+  SIMPLE_TOP_BAR: true,
   SIMPLE_TOP_BAR_CONTENT: process.env.NEXT_PUBLIC_THEME_SIMPLE_TOP_TIPS || '',
-  SIMPLE_LOGO_DESCRIPTION: process.env.NEXT_PUBLIC_THEME_SIMPLE_LOGO_DESCRIPTION || '<div>网安爱好者<br/>/资深干饭人<br/>/知识分享博主</div>',
-
+  SIMPLE_LOGO_DESCRIPTION:
+    process.env.NEXT_PUBLIC_THEME_SIMPLE_LOGO_DESCRIPTION ||
+    '<div>网安爱好者<br/>/资深干饭人<br/>/知识分享博主</div>',
   SIMPLE_AUTHOR_LINK: process.env.NEXT_PUBLIC_AUTHOR_LINK || '#',
-
-  SIMPLE_POST_AD_ENABLE: process.env.NEXT_PUBLIC_SIMPLE_POST_AD_ENABLE || false, // 文章列表是否插入广告
-
-  SIMPLE_POST_COVER_ENABLE: process.env.NEXT_PUBLIC_SIMPLE_POST_COVER_ENABLE || false, // 是否展示博客封面
-
-  SIMPLE_ARTICLE_RECOMMEND_POSTS: process.env.NEXT_PUBLIC_SIMPLE_ARTICLE_RECOMMEND_POSTS || true, // 文章详情底部显示推荐
-
-  // 菜单配置
-  SIMPLE_MENU_CATEGORY: true, // 显示分类
-  SIMPLE_MENU_TAG: true, // 显示标签
-  SIMPLE_MENU_ARCHIVE: true, // 显示归档
-  SIMPLE_MENU_SEARCH: true // 显示搜索
+  SIMPLE_POST_AD_ENABLE: process.env.NEXT_PUBLIC_SIMPLE_POST_AD_ENABLE || false,
+  SIMPLE_POST_COVER_ENABLE: process.env.NEXT_PUBLIC_SIMPLE_POST_COVER_ENABLE || false,
+  SIMPLE_ARTICLE_RECOMMEND_POSTS:
+    process.env.NEXT_PUBLIC_SIMPLE_ARTICLE_RECOMMEND_POSTS || true,
+  SIMPLE_MENU_CATEGORY: true,
+  SIMPLE_MENU_TAG: true,
+  SIMPLE_MENU_ARCHIVE: true,
+  SIMPLE_MENU_SEARCH: true,
 }
-export const CONFIG = {
-  MUSIC: {
-    ENABLED: true,
-    URL: 'https://raw.githubusercontent.com/xyx5916/audio-host/main/66765c5cdacc48af682833601b8eaaa9(1) - 副本.mp3',
-    AUTO_PLAY: true,
-    LOOP: true,
-    VOLUME: 0.3
-  }
+
+const MUSIC = {
+  ENABLED: true,
+  URL: 'https://raw.githubusercontent.com/xyx5916/audio-host/main/66765c5cdacc48af682833601b8eaaa9(1) - 副本.mp3',
+  AUTO_PLAY: true,
+  LOOP: true,
+  VOLUME: 0.3,
+}
+
+const CONFIG = {
+  ...SIMPLE_CONFIG,
+  MUSIC,
+}
+
 export default CONFIG
+export { CONFIG, MUSIC }
